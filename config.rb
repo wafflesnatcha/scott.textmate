dfe30a45a8d9b5afe9c4b0287768ecbe90717ee4
Scott Buchanan <buchanan.sc@gmail.com>
2012-03-08 06:51:04 -0800

    Converting to Compass (http://compass-style.org/)

---
+++ config.rb
@@ -0,0 +1,9 @@
+sass_path = File.dirname(__FILE__)
+css_dir = ""
+images_dir = "images"
+
+relative_assets = true
+
+environment = :production
+# environment = :development
+output_style = :expanded
